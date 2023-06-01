ba39e3c6f5fee028ea860c03b71a6678e60b25ee
tabydabi <kbroun@yandex.by>
2023-06-01 19:35:56 +0200

    fix:limit

--- dist/showdown.js
+++ dist/showdown.js
@@ -3687,7 +3687,7 @@ showdown.subParser('makehtml.unhashHTMLSpans', function (text, options, globals)
       var num = RegExp.$1;
       repText = repText.replace('¨C' + num + 'C', globals.gHtmlSpans[num]);
       if (limit === 60) {
-        console.error('maximum nesting of 10 spans reached!!!');
+        console.error('maximum nesting of 60 spans reached!!!');
         break;
       }
       ++limit;
